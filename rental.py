5af306ba21408b36994e272e4d2da81c2b08c2d7
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-16 16:19:55 +0700

    Finish refactoring 1-5

--- rental.py
+++ rental.py
@@ -28,29 +28,9 @@ class Rental:
 		return self.days_rented
 
 	def get_price(self):
-		amount = 0
-		if self.get_movie().get_price_code() == Movie.REGULAR:
-			# Two days for $2, additional days 1.50 each.
-			amount = 2.0
-			if self.get_days_rented() > 2:
-				amount += 1.5 * (self.get_days_rented() - 2)
-		elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-			# Three days for $1.50, additional days 1.50 each.
-			amount = 1.5
-			if self.get_days_rented() > 3:
-				amount += 1.5 * (self.get_days_rented() - 3)
-		elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-			# Straight per day charge
-			amount = 3 * self.get_days_rented()
-		else:
-			log = logging.getLogger()
-			log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
-		return amount
+		return self.get_movie().get_price_code().price(self.days_rented)
 
 	def get_renter_point(self):
 		frequent_renter_points = 0
-		if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-			frequent_renter_points += self.get_days_rented()
-		else:
-			frequent_renter_points += 1
+		frequent_renter_points += self.get_movie().get_price_code().frequent_renter_point(self.days_rented)
 		return frequent_renter_points
