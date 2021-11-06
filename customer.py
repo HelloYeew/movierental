de5ab916b73d38bce47dc5fac00205c50d0b2e8d
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-06 17:39:51 +0700

    Fix customer cannot run

--- customer.py
+++ customer.py
@@ -79,8 +79,8 @@ class Customer:
 if __name__ == "__main__":
     customer = Customer("Edward Snowden")
     print(customer.statement())
-    movie = Movie("Hacker Noon", Movie.REGULAR)
+    movie = Movie("Hacker Noon", "2021", ["Comedy"])
     customer.add_rental(Rental(movie, 2))
-    movie = Movie("CitizenFour", Movie.NEW_RELEASE)
+    movie = Movie("CitizenFour", "2005", ["Regular"])
     customer.add_rental(Rental(movie, 3))
     print(customer.statement())
